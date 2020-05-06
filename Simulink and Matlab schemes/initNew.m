ad66152535ea2d17f98187c02a9511ab00dc5a12
aDolinar <ad5062@student.uni-lj.si>
2020-05-06 20:38:45 +0200

    added Examples folder

--- Simulink and Matlab schemes/initNew.m
+++ Simulink and Matlab schemes/initNew.m
@@ -1,3 +1,7 @@
 %PD regulator Values
 Kp = 10;
 Kd = 0.1;
+load("misc\LazyFunctionQ\recorded.mat");
+pts=single(pts);
+accMax=[15,7.5,10,12.5,15,20,20]';
+system("ControlBlock_NEW.slx");
--- Simulink and Matlab schemes/misc/LazyFunctionQ/lazyFunction.m
+++ Simulink and Matlab schemes/misc/LazyFunctionQ/lazyFunction.m
@@ -1,10 +1,7 @@
 %load
 close all
 clc
-x=out.xOut;
-xDot=out.xDotOut;
-t=out.time;
-q=out.qOut;
+t=time;
 %set params
 maxDelta=0.001 %movement threshold for when the robot is considered static, default=0.001
 minS=3  %minimal time[s] the robot needs to stand still to record the point, default=3
@@ -12,28 +9,27 @@ minCount=100*minS;
 %filter data
 Wn = 5/100; % Normalized cutoff frequency
 [Fb,Fa]=butter(4,Wn);
-xDot=filter(Fb,Fa,xDot);
-x=filter(Fb,Fa,x);
+qDot=filter(Fb,Fa,qDotOut);
+q=filter(Fb,Fa,qOut);
 %%
 %plot
 figure();
 subplot(2,1,1);
-plot(t,x);
+plot(t,q);
 title("position");
-legend('x','y','z','eulerZ','eulerY','eulerX');
+legend('q1','q2','q3','q4','q5','q6','q7');
 subplot(2,1,2);
-plot(t,xDot);
+plot(t,qDot);
 title("velocity");
-legend('x','y','z','eulerZ','eulerY','eulerX');
+legend('q1','q2','q3','q4','q5','q6','q7');
 text=strcat("position and velocity");
 sgtitle(text,'FontSize',14);
-set(gcf, 'Position', get(0, 'Screensize'));
 %%
 %find stopped points
 clear idx
 found=0;
 for idx=1:length(t)
-    if mean(xDot(idx,:))<maxDelta
+    if mean(qDot(idx,:))<maxDelta
         if idx==1
             count(idx)=1;
         else
@@ -45,13 +41,13 @@ for idx=1:length(t)
     
 end
 count(idx+1)=0;
-pts=zeros(1,6);
+pts=zeros(1,7);
 %isolate maximums
 for idx=minCount:length(t)+1
     if abs(count(idx)-count(idx-1))>minCount-1
         found=found+1;
         idxFound(found)=idx-1;
-        pts(found,:)=x(idx-1,:);
+        pts(found,:)=q(idx-1,:);
     end
 end
 %plot found points
@@ -60,3 +56,9 @@ countVec=minCount*ones(length(t),1);
 plot(t,count([1:length(count)-1]),t,countVec,t(idxFound),count(idxFound),'o');
 legend('count','threshold','found Points');
 pts
+save("recorded.mat","pts");
+% for ii=1:size(pts,1)
+%     for jj=1:size(pts,2)
+%         
+%     end
+% end
